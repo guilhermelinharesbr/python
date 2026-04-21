79c93c36aa27695329e9e146fc83a4ae5a1aa490
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-21 16:56:21 -0300

    update file name

--- codes/treino-variaveis.py
+++
@@ -1,6 +0,0 @@
-sobrenome='Linhares'
-cidade='Fortaleza'
-estado='Ceará'
-ddd=85
-peso=69.5
-print(sobrenome,cidade,estado,ddd,peso)
---
+++ codes/variaveis-funcoes-print_e_input.py
@@ -0,0 +1,12 @@
+sobrenome='Linhares'
+cidade='Fortaleza'
+estado='Ceará'
+ddd=85
+peso=69.5
+print(sobrenome,cidade,estado,ddd,peso)
+#Agora será mostrado como ser interativo com o usuário
+sobrenome=input('Qual o seu sobrenome?')
+cidade=input('Qual a sua cidade?')
+ddd=input('Qual o seu DDD?')
+peso=input('Qual o seu peso?')
+print(sobrenome,cidade,estado,ddd,peso)
