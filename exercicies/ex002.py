445eb5e4067731a449ec1f463cc8cca6291ebb57
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-21 19:01:05 -0300

    add new files and folder rename folder

---
+++ exercicies/ex002.py
@@ -0,0 +1,5 @@
+#Crie um script Python que leia o nome
+#de uma pessoa e mostre uma mensagem de
+#prazer em conhecer de acordo com o valor digitado.
+nome=input('Qual o seu nome?')
+print('Prazer em te conhecer',nome,'!')
