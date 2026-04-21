a0240430e32a763eb64294d5741bd7a0c86288b9
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-21 17:34:02 -0300

    add files desafio02 and desafio03

---
+++ scripts/desafio02.py
@@ -0,0 +1,7 @@
+#Crie um script Python que leia o dia,
+#o mês e o ano de nascimento de uma pessoa
+#e mostre uma mensagem com a data formatada.
+dia=input('Qual o seu Dia de nascimento?')
+mes=input('Qual o seu Mês de nascimento?')
+ano=input('Qual o seu Ano de nascimento?')
+print('Sua data de nascimento é:',dia,'/',mes,'/',ano,'!')
---
+++ scripts/desafio03.py
@@ -0,0 +1,6 @@
+#Crie um script Python que leia dois números
+#e tente mostrar a soma entre eles.
+numero1=float(input('Digite o primeiro número:'))
+numero2=float(input('Digite o segundo número:'))
+soma=numero1+numero2
+print('A soma é:',soma)
