c41a38de52df6c773978ae06b79ddd0b340c4a93
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-21 19:18:06 -0300

    update ex002.py

--- exercicies/ex002.py
+++ exercicies/ex002.py
@@ -3,3 +3,6 @@
 #prazer em conhecer de acordo com o valor digitado.
 nome=input('Qual o seu nome?')
 print('Prazer em te conhecer',nome,'!')
+#Segunda forma
+nome2=input('Qual o seu nome?')
+print('Prazer em te conhecer,{}!'.format(nome2))
