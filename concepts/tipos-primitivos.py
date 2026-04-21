e5e5c95c5b1b596eae7881c30c6c398408c6a347
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-21 20:49:53 -0300

    add new folder named concepts

---
+++ concepts/tipos-primitivos.py
@@ -0,0 +1,5 @@
+#Tipos Primitivos
+#int   -> integer -> Ex: 7, -4, 0, 9578
+#float -> float   -> Ex: 7.0, 4.5, 0.076, -15.223
+#bool  -> boolean -> Ex: True, False. Obs: Sempre que for usar True e False, colocar a primeira letra maiúscula.
+#str   -> string  -> Ex: 'Olá', '7.5', '' Obs: o último exemplo é de uma string vazia.
