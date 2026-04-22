ed9ddbcfc891f20b59f56af49618cc89a63e6d27
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-22 19:25:41 -0300

    add new files folder concepts

---
+++ concepts/operadores-aritmeticos.py
@@ -0,0 +1,25 @@
+# Operadores Aritméticos:
+# + Adição -> Ex: 5+2==7
+# - Subtração -> Ex: 5-2==3
+# * Multiplicação -> Ex: 5*2==10
+# / Divisão -> Ex: 5/2==2.5
+# ** Potência -> Ex: 5**2==25
+# // Divisão Inteira -> Ex: 5//2==2
+# % Resto da Divisão -> Ex: 5%2==1
+# == Igual
+# = Atribuição ou também conhecido com Recebe.
+
+#Ordem de Precedência dos Operadores Aritméticos:
+# 1º são os parênteses -> ()
+# 2º são as potências -> **
+# 3º são a multiplicação '*', divisão '/', divisão inteira '//', resto da divisão '%' -> 
+# 4º Soma e Subtração -> + e -
+
+#####################
+
+#Concatena as str Oi e Olá
+print('Oi'+'Olá')
+#Imprime a str oi 3 vezes
+print('Oi'*3)
+#Imprime o símbolo do = 15 vezes
+print('='*15)
