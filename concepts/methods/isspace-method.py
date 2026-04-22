a9e5abadab2a485834be586f489b58461c3b6912
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-22 19:38:14 -0300

    add new folder named methods

---
+++ concepts/methods/isspace-method.py
@@ -0,0 +1,5 @@
+#O método isspace() retorna True se todos os caracteres 
+#em uma string forem espaços em branco, caso contrário, 
+#retorna False.
+n=input('Digite algo: ')
+print(n.isspace())
---
+++ concepts/methods/istitle-method.py
@@ -0,0 +1,9 @@
+#O método istitle() retorna `True` se todas as palavras 
+#em um texto começarem com letra maiúscula E o restante 
+#da palavra for composto por letras minúsculas; caso 
+#contrário, retorna False.
+#Símbolos e números são ignorados.
+#Também pode ser chamado aqui no Brasil com Capitalizado.
+n=input('Digite algo: ')
+print(n.istitle())
+print(n.isspace)
