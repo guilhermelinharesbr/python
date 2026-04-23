2712695e63c1b294422eb5be19e27858c63bf51c
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-23 20:26:36 -0300

    edit desafio006

--- exercises/desafio005.py
+++ exercises/desafio005.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia um número inteiro
 #e mostre na tela o seu sucessor e seu antecessor.
 
-#Primeira resposta
+#Primeira forma, usando várias variáveis
 numero = int(input('Digite um número:'))
 sucessor=numero+1
 antecessor=numero-1
@@ -9,6 +9,6 @@ print('O número digitado foi {}, o seu sucessor é {}, o seu antecessor é {}.'
 
 ##############
 
-#Segunda resposta
+#Segunda forma, usando apenas uma variável
 numero = int(input('Digite um número:'))
 print('O número digitado foi {}, o seu sucessor é {}, o seu antecessor é {}.'.format(numero,(numero+1),(numero-1)))
--- exercises/desafio006.py
+++ exercises/desafio006.py
@@ -1,2 +1,19 @@
 #Crie um algoritmo que leia um número e mostre o seu
 #dobro, triplo e raiz quadrada.
+
+#Primeira forma, usando várias variáveis
+numero=int(input('Digite um número:'))
+dobro=numero*2
+triplo=numero*3
+raizquadrada=numero ** (1/2)
+print('O número digitado foi {}, o seu dobro é {}, o seu triplo é {} e sua raiz quadrada é {}'.format(numero,dobro,triplo,raizquadrada))
+
+##########
+
+#Segunda forma, usando apenas uma variável
+numero=int(input('Digite um número:'))
+print('O número digitado foi {}\n O seu dobro é {}\n O seu triplo é {}\n A raiz quadrada é {:.2f}'.format(numero,(numero*2),(numero*3),(numero ** (1/2))))
+
+#Terceira forma, usando a pow function
+numero=int(input('Digite um número:'))
+print('O número digitado foi {}\n O seu dobro é {}\n O seu triplo é {}\n A raiz quadrada é {:.2f}'.format(numero,(numero*2),(numero*3),(pow(numero,1/2))))
