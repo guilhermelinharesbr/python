8878facece7d1aa2e5f876ee71dcf171309798e3
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-23 19:59:10 -0300

    edit desafio005

--- exercises/desafio005.py
+++ exercises/desafio005.py
@@ -1,2 +1,6 @@
 #Faça um programa que leia um número inteiro
 #e mostre na tela o seu sucessor e seu antecessor.
+numero = int(input('Digite um número:'))
+sucessor=numero+1
+antecessor=numero-1
+print('O número digitado foi {}, o seu sucessor é {}, o seu antecessor é {}.'.format(numero,sucessor,antecessor))
