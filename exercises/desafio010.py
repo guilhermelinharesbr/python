8266759d9d8a6132f863c0df0be1e259e6f4135b
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-24 13:16:56 -0300

    edit desafio010

--- exercises/desafio010.py
+++ exercises/desafio010.py
@@ -1,3 +1,8 @@
 #Crie um programa que leia quanto dinheiro uma pessoa
 #tem na carteira e mostre quantos Dólares ela pode comprar.
 #Considere US$1.00 = R$3.27
+
+#Resposta
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 3.27
+print('Com R${:.2f} você pode comprar U${:.2f}'.format(real,(dolar)))
