27439b34156af26f449b41f84118f42b2fb77e68
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-24 13:37:00 -0300

    edit desafio011

--- exercises/desafio011.py
+++ exercises/desafio011.py
@@ -2,3 +2,18 @@
 #parede em metros, calcule a sua área e a quantidade de 
 #tinta necessária para pintá-la, sabendo que cada litro 
 #de tinta, pinta uma área de 2m².
+
+#Primeira forma
+largura = float(input('Largura da parede:'))
+altura = float(input('Altura da parede:'))
+area = largura * altura
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m²'.format(largura,altura,area))
+print('Para pintar essa parede, você precisará de {:.2f}l de tinta'.format(area/2))
+
+#Segunda forma
+largura = float(input('Largura da parede:'))
+altura = float(input('Altura da parede:'))
+área = largura * altura
+tinta = área / 2
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m²'.format(largura,altura,área))
+print('Para pintar essa parede, você precisará de {:.2f}l de tinta'.format(tinta))
