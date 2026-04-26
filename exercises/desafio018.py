9e0c752d0390f42a5384dd09155678060668f5b4
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-26 20:58:42 -0300

    edit file desafio018

--- exercises/desafio018.py
+++ exercises/desafio018.py
@@ -1,2 +1,10 @@
-#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
-#usar biblioteca para resolver
+"""Faça um programa que leia um ângulo qualquer e mostre 
+na tela o valor do seno, cosseno e tangente desse ângulo.
+
+#usar biblioteca para resolver. """
+
+#Primeira forma
+angulo = float(input('Digite o valor do ângulo:'))
+O ângulo de {} tem o SENO de {}
+O ângulo de {} tem o COSSENO de {}
+O ângulo de {} tem a TANGENTE de {}
