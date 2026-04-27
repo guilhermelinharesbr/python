e1e58e8cfa2c1a1b5dc24860ab4901e8b1844ef5
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-04-27 14:06:51 -0300

    edit file desafio020

--- exercises/desafio020.py
+++ exercises/desafio020.py
@@ -4,3 +4,27 @@ Faça um programa que leia o nome dos quatro alunos e
 mostre a orden sorteada."""
 
 #Primeira forma
+import random
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+""""O método shuffle da classe random embaralha os itens de uma
+lista. Em português shuffle significa embaralhar."""
+random.shuffle(lista)
+print('A ordem de apresentação será:')
+print(lista)
+
+#Segunda forma
+from random import shuffle
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+""""O método shuffle da classe random embaralha os itens de uma
+lista. Em português shuffle significa embaralhar."""
+shuffle(lista)
+print('A ordem de apresentação será:')
+print(lista)
