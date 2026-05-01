72aeff5f2351b1db1547796347dc35473324236f
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-01 20:33:08 -0300

    edit file desafio026

--- exercises/desafio026.py
+++ exercises/desafio026.py
@@ -7,3 +7,16 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 
 -> Em que posição ela aparece a última vez.
 """
+
+#Primeira forma
+frase = str(input('Digite uma frase: ')).strip()
+frase_maiuscula = frase.upper()
+print('A letra A aparece {} vezes na frase.'.format(frase_maiuscula.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(frase_maiuscula.find('A')))
+print('A última letra A apareceu na posição {}'.format(frase_maiuscula.rfind('A')))
+
+#Segunda forma
+frase = str(input('Digite uma frase: ')).strip().upper()
+print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+print('A primeira letra a apareceu na posição {}'.format(frase.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(frase.rfind('A')+1))
