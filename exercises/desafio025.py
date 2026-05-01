592d2eafd31394327f216e13be0910db3efdc908
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-01 19:32:06 -0300

    edit files

--- exercises/desafio025.py
+++ exercises/desafio025.py
@@ -1,3 +1,12 @@
 """
-Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
+Crie um programa que leia o nome de uma pessoa e 
+diga se ela tem "Silva" no nome.
 """
+
+#Primeira forma
+nome = str(input('Digite seu nome completo: ')).strip()
+print('SILVA' in nome.upper())
+
+#Segunda forma
+nome = str(input('Qual é seu nome completo?')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
