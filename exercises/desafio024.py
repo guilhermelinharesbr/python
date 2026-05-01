ed326eab08ccbfeed3a21e1868b6ef13ecffd073
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-01 18:47:14 -0300

    edit file

--- exercises/desafio024.py
+++ exercises/desafio024.py
@@ -1,3 +1,16 @@
 """
-Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo".
+Crie um programa que leia o nome de uma cidade e diga
+se ela começa ou não com o nome "Santo".
 """
+
+#Primeira forma
+cidade = str(input('Qual sua cidade de nascimento? '))
+print('Santo' in cidade)
+
+#Segunda forma
+cidade = str(input('Em que cidade você nasceu? '))
+print(cidade[:5] == 'Santo')
+
+#Terceira forma
+cidade = str(input('Em que cidade você nasceu? ')).strip()
+print(cidade[:5].upper == 'SANTO')
