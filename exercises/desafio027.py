3f1c8e941e2661be2ee69d0f69d198071e3f81bc
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-01 06:40:36 -0300

    add files

---
+++ exercises/desafio027.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que leia o nome completo de uma pessoa, mostrando em seguuida o primeiro e o último nome separadamente.
+
+Ex:Ana Maria de Souza
+primeiro:Ana
+último:Souza
+"""
