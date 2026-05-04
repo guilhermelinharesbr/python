b619d3c0adaea7e469e23017de3970eb8b775c4a
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-04 20:33:23 -0300

    edit desafio030

--- exercises/desafio030.py
+++ exercises/desafio030.py
@@ -1,3 +1,19 @@
 """
-Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+Crie um programa que leia um número inteiro e 
+mostre na tela se ele é PAR ou ÍMPAR.
 """
+
+#Primeira forma
+numero = int(input('Digite um número: '))
+if numero % 2 == 0:
+    print ('O número {} é PAR.'.format(numero))
+else:
+    print ('O número {} é ÍMPAR.'.format(numero))
+
+#Segunda forma
+numero = int(input('Me diga um número qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O número {} é PAR.'.format(numero))
+else:    
+    print('O número {} é ÍMPAR.'.format(numero))
