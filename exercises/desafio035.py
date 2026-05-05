afda0f0430749b669cc7a308f4573d4911ea9db2
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-05 14:02:42 -0300

    edit file desafio035

--- exercises/desafio035.py
+++ exercises/desafio035.py
@@ -1,3 +1,29 @@
 """
-Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não forma um triângulo.
+Desenvolva um programa que leia o comprimento de três
+retas e diga ao usuário se elas podem ou não forma um triângulo.
 """
+
+#Primeira forma
+print('-=-' * 20)
+print('Verificador de Triângulos')
+print('-=-' * 20)
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+if a + b > c and a + c > b and b + c > a:
+    print('Os segmentos PODEM formar um triângulo.')
+else:
+    print('Os segmentos NÃO podem formar um triângulo.')
+
+
+#Segunda forma
+print('-=-' * 20)
+print('Analisador de Triângulos')
+print('-=-' * 20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM formar um triângulo.')
+else:
+    print('Os segmentos acima NÃO podem formar um triângulo.')
