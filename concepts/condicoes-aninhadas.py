bc875d2facbf4cdbef3f7e94930c9cc48359e5c4
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-08 16:12:43 -0300

    edit file

---
+++ concepts/condicoes-aninhadas.py
@@ -0,0 +1,35 @@
+"""
+Condições aninhadas: 
+ Em Python permitem colocar estruturas if, 
+elif ou else dentro de outros blocos condicionais para verificar 
+múltiplas condições sequenciais. Elas exigem endentação correta 
+para definir o escopo de cada verificação. Embora úteis, o uso 
+excessivo pode prejudicar a legibilidade, sendo recomendado usar 
+operadores lógicos (and, or) para simplificar, quando possível.
+
+elif -> Em Python 'elif' é simplicação do nome da junção do else
+        com o if. 
+
+Obs: Sempre terá por um if e nunca poderá começar com um elif e 
+     nem sempre terá o else. O elif e o else são opcionais,
+     até mesmo quando usa o elif o else é opcional. 
+Obs2: Dentro de um if você pode utilizar quantos elif você quiser,
+      e dentro deste mesmo if o else pode ser utilizado apenas uma
+      vez ou nenhuma.
+      
+
+Ex:
+carro.siga()
+if carro.esquerda():
+    bloco1
+elif carro.direita():
+    bloc2
+elif carro.ré():
+    bloco3
+else:
+    bloco4
+
+
+"""
+
+PAREI EM 11 MIN da AULA 49
