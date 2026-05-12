1c279526f81c6fcefbb7036bb51a85c4820796e3
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-12 18:27:55 -0300

    edit file

--- exercises/desafio038.py
+++ exercises/desafio038.py
@@ -1,7 +1,29 @@
 """
-Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
+Escreva um programa que leia dois números inteiros e 
+compare-os, mostrando na tela uma mensagem:
 
 - O primeiro valor é maior
 - O segundo valor é maior
 - Não existe valor maior, os dois são iguais.
 """
+
+#Primeira forma
+primeiro_num = int(input('Digite o primeiro número: '))
+segundo_num = int(input('Digite o segundo número: '))
+if primeiro_num > segundo_num:
+    print('{} é maior que {}.'.format(primeiro_num,segundo_num))
+if segundo_num > primeiro_num:
+    print('{} é maior que {}.'.format(segundo_num, primeiro_num))
+if segundo_num == primeiro_num:
+    print('{} é igual a {}.'.format(primeiro_num,segundo_num))
+
+
+#Segunda forma
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior.')
+elif n2 > n1:
+    print('O SEGUNDO valor é maior.')
+else:
+    print('Os dois valores são IGUAIS.')    
--- exercises/desafio039.py
+++ exercises/desafio039.py
@@ -1,5 +1,6 @@
 """
-Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
+Faça um programa que leia o ano de nascimento de um jovem
+e informe, de acordo com sua idade:
 
 - Se ele ainda vai se alistar ao serviço militar.
 
@@ -7,5 +8,6 @@ Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo 
 
 - Se já passou do tempo do alistamento.
 
-Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+Seu programa também deverá mostrar o tempo que falta ou 
+que passou do prazo.
 """
