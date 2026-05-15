d773ffb3cb418bc2a9bc937bece52e13c3efe869
guilhermelinharesbr <guilhermelinhares.bkp@gmail.com>
2026-05-15 13:12:33 -0300

    edit file

--- exercises/desafio045.py
+++ exercises/desafio045.py
@@ -1,3 +1,14 @@
 """
 Crie um programa que faça o computador jogar Jokenpô com você.
 """
+
+#Primeira forma
+print('-=-' * 5, 'JO-KEN-PO', ('-=-' * 5) + '\n',
+"""
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA      
+""")
+
+
+#Segunda forma
