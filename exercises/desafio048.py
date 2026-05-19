1f2a79bdc64154de57fafaaa627e3fb9388c5b45
guilhermelinharesbr <guilhermelinhares.bkp@gmail.com>
2026-05-19 10:39:36 -0300

    modified desafio048 e desafio049

--- exercises/desafio048.py
+++ exercises/desafio048.py
@@ -2,8 +2,6 @@
 Faça um programa que calcule a soma entre todos os números 
 ímpares que são múltiplos de três e que se encontram no 
 intervalo de 1 até 500.
-
-"A soma de todos os 83 valores solicitados é 20667"
 """
 
 #Primeira forma
--- exercises/desafio049.py
+++ exercises/desafio049.py
@@ -1,3 +1,21 @@
 """
-Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+Refaça o DESAFIO 009, mostrando a tabuada de um número 
+que o usuário escolher, só que agora utilizando um laço for.
 """
+
+#Primeira forma
+valor = int(input('Digite um número para ver sua tabuada: '))
+cont = 0
+print('-'*12)
+for c in range(1, 11):
+    cont = cont + 1
+    print(valor, ' x ', (cont), ' = ', (valor * cont))
+
+print('=' * 20) 
+###############################
+
+#Segunda forma
+num = int(input('Digite um número para ver sua tabuada: '))
+print('-'*12)
+for c in range(1, 11):
+    print('{}  x  {:2} = {}'.format(num, c, num * c))
