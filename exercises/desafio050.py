fcc61151aaf77ca370cfa4be0cb9faf02effaa12
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-24 06:32:28 -0300

    edit desafio050

--- exercises/desafio050.py
+++ exercises/desafio050.py
@@ -1,3 +1,24 @@
 """
-Desenvolva um programa que leia seis números interiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+Desenvolva um programa que leia seis números inteiros
+e mostre a soma apenas daqueles que forem pares. 
+Se o valor digitado for ímpar, desconsidere-o.
 """
+
+#Primeira forma
+soma = 0
+for c in range (0, 6):
+    num = int(input('Digite um número: '))
+    if num % 2 == 0:
+        soma = soma + num
+print('A soma dos números pares digitados foi {}'.format(soma)) 
+
+
+#Segunda forma
+soma = 0
+cont = 0
+for c in range (1, 7):
+    num = int(input('Digite o {}º valor: '.format(c)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Você informou {} números PARES e  a soma foi {}'.format(cont, soma))
