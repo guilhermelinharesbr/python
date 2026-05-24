925aaae5d1c5637d794891fc3af108ce8dfa20f5
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-24 06:32:40 -0300

    edit desafio050

--- exercises/desafio050.py
+++ exercises/desafio050.py
@@ -10,7 +10,7 @@ for c in range (0, 6):
     num = int(input('Digite um número: '))
     if num % 2 == 0:
         soma = soma + num
-print('A soma dos números pares digitados foi {}'.format(soma)) 
+print('A soma dos número(s) pares digitados foi {}'.format(soma)) 
 
 
 #Segunda forma
