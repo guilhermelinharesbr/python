5fe5cc812f3d00684258eee0beac1d7900da3792
guilhermelinharesbr <guilhermelinhares.bkp@gmail.com>
2026-05-25 11:26:08 -0300

    edit desafio051

--- exercises/desafio051.py
+++ exercises/desafio051.py
@@ -1,3 +1,25 @@
 """
-Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. 
+No final, mostre os 10 primeiros termos dessa progressão.
 """
+
+#Primeira forma
+print('=' * 20)
+print('10 TERMOS DE UMA PA')
+print('=' * 20)
+primeiro_termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+pa = primeiro_termo
+print(pa, '->', end=' ')
+for c in range (0, 9):
+  pa = pa + razao 
+  print(pa, '->', end=' ')
+print('FIM')
+
+#Segunda forma
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+  print('{} '.format(c), end='-> ')
+print('ACABOU')
