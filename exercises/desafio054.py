e6e5ab2bc0012e9f1fe4a6348204c5568d748162
Carlos Guilherme Linhares Silva <guilherme.linhares@dnocs.gov.br>
2026-05-31 05:26:09 -0300

    modified desafio054

--- exercises/desafio054.py
+++ exercises/desafio054.py
@@ -1,5 +1,16 @@
 """
-Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+Crie um programa que leia o ano de nascimento de sete pessoas. 
+No final, mostre quantas pessoas ainda não atingiram a maioridade
+e quantas já são maiores.
 
 Obs: considerando a maior idade sendo 21 anos.
 """
+
+#Primeira forma
+
+for c in range (0, 7, 1):
+    idade = int(input('Em que ano a {} ª pessoa nasceu? ').format(c))
+print('Ao foram {} pessoas'.format(c)) 
+
+
+#Segunda forma
