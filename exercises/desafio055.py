c2fe70fe91ee6ac0b6cb03cfc911c895f339ea6e
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-31 10:26:38 -0300

    modified desafio055.py

--- exercises/desafio055.py
+++ exercises/desafio055.py
@@ -1,3 +1,24 @@
 """
-Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+Faça um programa que leia o peso de cinco pessoas. 
+No final, mostre qual foi o maior e o menor peso lidos.
 """
+
+#Primeira forma
+maior_peso = 0
+menor_peso = 0
+for pessoa in range (1, 6):
+    peso = float(input('Qual o peso da {} pessoa ? '.format(pessoa)))
+    if pessoa == 1:
+        maior_peso = peso
+        menor_peso = peso
+    else:
+        if peso > maior_peso:
+            maior_peso = peso
+        if peso < menor_peso:
+            menor_peso = peso
+
+print(f'O Peso é {peso}')
+print(f'O Maior peso é {maior_peso}')
+print(f'O Menor peso é {menor_peso}')
+
+#Segunda forma
