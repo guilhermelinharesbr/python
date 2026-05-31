b65cc6cfe1e0048ddcf99c177ebe862d75522425
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-05-31 14:49:30 -0300

    modified desafio55

--- exercises/desafio055.py
+++ exercises/desafio055.py
@@ -2,7 +2,7 @@
 Faça um programa que leia o peso de cinco pessoas. 
 No final, mostre qual foi o maior e o menor peso lidos.
 """
-
+"""
 #Primeira forma
 maior_peso = 0
 menor_peso = 0
@@ -20,5 +20,19 @@ for pessoa in range (1, 6):
 print(f'O Peso é {peso}')
 print(f'O Maior peso é {maior_peso}')
 print(f'O Menor peso é {menor_peso}')
-
+"""
 #Segunda forma
+maior = 0
+menor = 0
+for p in range(1, 6):
+    peso = float(input('Peso da {} pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}Kg'.format(maior))
+print('O menor peso lido foi de {}Kg'.format(menor))
