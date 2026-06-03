8966a626e3d1cbde6b96bcd6af346b6a0899796d
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-06-03 12:43:10 -0300

    modified desafio058

--- exercises/desafio058.py
+++ exercises/desafio058.py
@@ -6,25 +6,41 @@ final quantos palpites foram necessários para vencer.
 """
 
 #Primeira forma
+from random import randint
+from time import sleep
+cont = 0
+jogador = 11
+computador = randint(0,10) 
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
+jogador = int(input('Em que número eu pensei? '))
+cont += 1
+print('PROCESSANDO...')
+sleep(2)
+while jogador != computador:
+    jogador = int(input('Tente novamente: '))
+    cont += 1
+    print('PROCESSANDO...')
+    sleep(2)
+if jogador == computador:
+    print(f'PARABÉNS! YOU WIN! Eu pensei no número {computador} e você levou {cont} palpites para acertar que eu pensei no número {jogador}.')
 
 
 #Segunda forma
-
-
-
-"""
 from random import randint
-from time import sleep
-#Escolhe um número de maneira aleatória de 0 a 5:
-computador = randint(0,5) 
-print('-=-' * 20)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
-jogador = int(input('Em que número eu pensei? '))
-print('PROCESSANDO...')
-#O método sleep do módulo time, faz o PC ficar parado por 3 segundos:   
-sleep(3)  
-if jogador == computador:
-    print('PARABÉNS! YOU WIN!')
-else:
-    print('GANHEI! Eu pensei no número {} e não no {}!'.format(computador, jogador))
-"""    
+computador = randint(0,10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi? ')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas. Parabéns!'.format(palpites)) 
