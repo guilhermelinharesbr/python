3b997e8cd086265bdb0a90bc83f914326c55d3f3
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-06-06 10:09:32 -0300

    modified desafio059

--- exercises/desafio059.py
+++ exercises/desafio059.py
@@ -11,19 +11,19 @@ em cada caso.
 """
 
 #Primeira forma
+
 opcao = 0
 
 valor1 = int(input('Primeiro valor: '))
 valor2 = int(input('Segundo valor: '))
 
 while opcao != 5:
-    print(f""" --------
-    [1] Somar
-    [2] Multiplicar
-    [3] Maior
-    [4] Novos números
-    [5] Sair do programa
-    """)
+#    print(f'''-------- 
+#    [1] Somar
+#    [2] Multiplicar
+#    [3] Maior
+#    [4] Novos números
+#    [5] Sair do programa''')
     opcao = int(input(f'Qual é a sua opção? '))
     if opcao == 1:
         soma = valor1 + valor2
@@ -50,11 +50,40 @@ while opcao != 5:
         print('Opção inválida. Tente novamente.')
 print(f'Fim do Programa.')
 
-
+#################
             
-
-
-
-
-
 #Segunda forma
+from time import sleep
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opcao = 0
+while opcao != 5:
+    print('''   [1] somar
+        [2] multiplicar
+        [3] maior
+        [4] novos números
+        [5] sair do programa''')
+    opcao = int(input('>>>>>> Qual é a sua opcão? '))
+    if opcao == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}'.format(n1, n2, soma))
+    elif opcao == 2:
+        produto = n1 * n2
+        print('O resultado de {} x {} é {}'.format(n1, n2, produto))
+    elif opcao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior valor é {}'.format(n1, n2, maior))
+    elif opcao == 4:
+        print('Informe os números novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida tente novamente.')
+    print('=-=' * 10)
+    sleep(2)
+print('Fim do programa. Volte sempre!')
