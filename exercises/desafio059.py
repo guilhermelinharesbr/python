0256a7f8bc1de506c92a4bbd7e8218cacf6f9818
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-06-06 09:11:53 -0300

    Primeira forma desafio049

--- exercises/desafio059.py
+++ exercises/desafio059.py
@@ -11,8 +11,50 @@ em cada caso.
 """
 
 #Primeira forma
+opcao = 0
 
 valor1 = int(input('Primeiro valor: '))
 valor2 = int(input('Segundo valor: '))
+
+while opcao != 5:
+    print(f""" --------
+    [1] Somar
+    [2] Multiplicar
+    [3] Maior
+    [4] Novos números
+    [5] Sair do programa
+    """)
+    opcao = int(input(f'Qual é a sua opção? '))
+    if opcao == 1:
+        soma = valor1 + valor2
+        print(f'A soma de {valor1} e {valor2} é \033[32m{soma}\033[0m.')
+    elif opcao == 2:
+        multiplicacao = valor1 * valor2
+        print(f'A multiplicação de {valor1} e {valor2} é \033[32m{multiplicacao}\033[0m.')
+    elif opcao == 3:
+        if valor1 > valor2:
+            maior = valor1
+            print(f'Entre {valor1} e {valor2} o maior valor é \033[32m{maior}\033[0m.')
+        elif valor1 < valor2:
+            maior = valor2
+            print(f'Entre {valor1} e {valor2} o maior valor é \033[32m{maior}\033[0m.')
+        else:
+            print(f'Entre {valor1} e {valor2} não existe maior valor, pois ambos são iguais.')
+    elif opcao == 4:
+        print(f'Informe os novos números: ')
+        valor1 = int(input('Primeiro valor: '))
+        valor2 = int(input('Segundo valor: '))
+    elif opcao == 5:
+        print(f'\033[32mFinalizando...\033[0m')
+    else:
+        print('Opção inválida. Tente novamente.')
+print(f'Fim do Programa.')
+
+
+            
+
+
+
+
 
 #Segunda forma
