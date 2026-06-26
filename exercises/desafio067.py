23f5f78ff04afb600561da7921face7b77b7570a
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-06-26 15:44:25 -0300

    add files

---
+++ exercises/desafio067.py
@@ -0,0 +1,14 @@
+"""
+Faça um programa que mostre a tabuada de vários
+números, um de cada vez, para cada valor digitado
+pelo usuário. O programa será interrompido quando
+o número for negativo.
+"""
+
+#Primeira forma
+
+
+
+#################
+
+#Segunda forma
