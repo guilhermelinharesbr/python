3f9a1e1946070379b5575422f20f1b9423a130d9
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-07-01 15:57:30 -0300

    modified: desafio072.py

--- exercises/desafio072.py
+++ exercises/desafio072.py
@@ -1,14 +1,44 @@
 """
 Crie um programa que tenha uma tupla totalmente
-preenchida com uma contagem por exntenso de zero 
+preenchida com uma contagem por extenso de zero 
 até vinte.
 Seu programa deverá ler um número pelo teclado
 (entre 0 e 20) e mostrá-lo por extenso.
 """
 
 #Primeira forma
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+           'seis', 'sete', 'oito', 'nove', 'dez',
+           'onze', 'doze', 'treze', 'catorze', 'quinze',
+           'dezesseis', 'dezesete', 'dezeoito', 'dezenove', 'vinte')
+
+while True:
+    while True:
+        numero = int(input('Digite um número entre 0 e 20: '))
+        if numero >= 0 and numero <= 20:
+            print(f'Você digitou o número {numeros[numero]}')
+            break
+        else:
+            print(f'Tente novamente. Digite um número válido!')
+    
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+
 
 #################
 
 
 #Segunda forma
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+           'seis', 'sete', 'oito', 'nove', 'dez',
+           'onze', 'doze', 'treze', 'catorze', 'quinze',
+           'dezesseis', 'dezesete', 'dezeoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {cont[num]}')
