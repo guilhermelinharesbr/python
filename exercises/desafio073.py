3f9e667afe5a4e466e4bdf3b61d0f337ba4b0a28
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-07-02 09:41:07 -0300

    modified: desafio073.py

--- exercises/desafio073.py
+++ exercises/desafio073.py
@@ -13,8 +13,32 @@ D) Em que posição na tabela está o time da Chapecoense.
 """
 
 #Primeira forma
+brasileirao_2021 = ('Atlético MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians',
+                     'Bragantino','Fluminense', 'América MG', 'Atlético GO', 'Santos', 
+                     'Ceará', 'Internacional', 'São Paulo', 'Athletico PR', 'Cuiabá',
+                     'Juventude', 'Grêmio', 'Bahia', 'Sport', 'Chapecoense')
+
+print(f'Os 5 primeiros colocados do Brasileirão 2021 foram: {brasileirao_2021[0:5]}')
+print(f'Os 4 últimos colocados do Brasileirão 2021 foram: {brasileirao_2021[16:20]}')
+print(f'As equipes do Brasileirão 2021 em ordem alfabética são: {sorted(brasileirao_2021)}')
+print(f'A equipe da Chapecoense ficou em {brasileirao_2021.index("Chapecoense")+1}º do Brasileirão 2021.')
+
 
 #################
 
 
 #Segunda forma
+times = ('Atlético MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians',
+                     'Bragantino','Fluminense', 'América MG', 'Atlético GO', 'Santos', 
+                     'Ceará', 'Internacional', 'São Paulo', 'Athletico PR', 'Cuiabá',
+                     'Juventude', 'Grêmio', 'Bahia', 'Sport', 'Chapecoense')
+print('-=' * 15)
+print(f'Lista de times do Brasileirão de 2021: {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros são: {times[0:5]}')
+print('-=' * 15)
+print(f'Os 4 últimos são: {times[-4:]}')
+print('-=' * 15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-=' * 15)
+print(f'A Chapecoense está na {times.index("Chapecoense")+1}ª posição')
