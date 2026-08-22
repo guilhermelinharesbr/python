8de191345ab3b64413cba7c422b9734de9cd00ea
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-08-22 11:53:04 -0300

    modified: update desafio017.py

--- exercises/desafio017.py
+++ exercises/desafio017.py
@@ -1,4 +1,8 @@
-#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo, calcule e mostre o comprimento da hipotenusa.
+"""
+Faça um programa que leia o comprimento do cateto oposto 
+e do cateto adjacente de um triângulo, calcule e mostre 
+o comprimento da hipotenusa.
+"""
 
 #Primeira forma
 import math
