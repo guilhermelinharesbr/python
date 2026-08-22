dd2709adaad9bcf6f1b7478e69d89b6d01cecc59
Guilherme Linhares <guilhermelinhares.bkp@gmail.com>
2026-08-22 11:52:02 -0300

    modified: update desafio015.py

--- exercises/desafio015.py
+++ exercises/desafio015.py
@@ -1,7 +1,9 @@
-#Escreva um programa que pergunte a quantidade de Km
-#percorridos por um carro alugado e a quantidade de dias
-#pelos quais ele foi alugado. Calcule o preço a pagar,
-#sabendo que o carro custa R$60,00 por dia e R$0,15 por Km rodado.
+"""
+Escreva um programa que pergunte a quantidade de Km
+percorridos por um carro alugado e a quantidade de dias
+pelos quais ele foi alugado. Calcule o preço a pagar,
+sabendo que o carro custa R$60,00 por dia e R$0,15 por Km rodado.
+"""
 
 #Primeira forma
 dias = int(input('Por quantos dias o carro foi alugado?'))
